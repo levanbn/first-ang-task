a46042edb9b83874c833ff5d6b7c03ce4ea9e028
levani <levancho.begashvili@gmail.com>
2023-01-21 19:31:58 +0400

    fgdgfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 
+interface Options {
+  id: number;
+  name: Dificulty;
+}
+
+enum Dificulty {
+  Easy = 'Easy',
+  Medium = 'Medium',
+  Hard = 'Hard',
+}
+
+
+
+enum BtnColors {
+  Green = "green",
+  Red = "red"
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first-ang-task';
+
+  options: Array<Options> = [{ id: 1, name: Dificulty.Easy },{ id: 2, name: Dificulty.Medium },{ id: 3, name: Dificulty.Hard }];
+
+  dificulty = Dificulty;
+  title = 'todo-app';
+  list: any = [];
+  optName: string = this.dificulty.Easy;
+
+  taskname: string = '';
+  secondlist: any[] = [];
+  thirdlist: any[] = [];
+
+
+  buttoncolors = [BtnColors.Green,BtnColors.Red];
+
+
+  addtask(x: string) {
+    if (x !== '') {
+      this.list.push({ id: this.list.length, name: x, opts: this.optName });
+      this.taskname = '';
+    }
+  }
+  removetask(id: number) {
+    this.list.splice(id, 1);
+  }
+  addtoProgress(x: string, id: number) {
+    this.secondlist.push({ id: this.secondlist.length, name: x, opts: this.optName  });
+    this.list.splice(id, 1);
+  }
+  addtoDone(x: string, id: number) {
+    this.thirdlist.push({ id: this.thirdlist.length, name: x, opts: this.optName  });
+    this.secondlist.splice(id, 1);
+  }
+  addfromDonetoProgress(x: string, id: number) {
+    this.secondlist.push({ id: this.thirdlist.length, name: x, opts: this.optName  });
+    this.thirdlist.splice(id, 1);
+  }
+  secondToFirst(x: string, id: number) {
+    this.list.push({ id: this.list.length + 1, name: x, opts: this.optName  });
+    this.secondlist.splice(id, 1);
+  }
 }
+
